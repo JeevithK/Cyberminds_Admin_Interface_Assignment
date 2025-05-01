56fc7ac27670eaaf20f364a368f21fa7c9795cd6
Jeevith K <104653693+JeevithK@users.noreply.github.com>
2025-05-01 14:59:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://cyberminds-admin-interface-assignment-bamg.onrender.com",
     credentials: true,
   })
 );
