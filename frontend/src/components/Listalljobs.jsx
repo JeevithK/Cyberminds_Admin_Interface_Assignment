3b0399167c770e53c35cf2b37b25e2a947a95646
Jeevith K <104653693+JeevithK@users.noreply.github.com>
2025-05-02 01:31:04 +0530

    Update Listalljobs.jsx

--- frontend/src/components/Listalljobs.jsx
+++ frontend/src/components/Listalljobs.jsx
@@ -50,7 +50,7 @@ useEffect(() => {
   const fetchJobs = async () => {
     setIsLoading(true);
     try {
-      const res = await axios.get("http://localhost:5000/admin/getalljobs");
+      const res = await axios.get("https://cyberminds-admin-interface-assignment-bamg.onrender.com/admin/getalljobs");
       setAllJobs(res.data);
     } catch (error) {
       console.error("Error fetching jobs:", error);
